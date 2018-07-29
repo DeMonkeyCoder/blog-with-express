90a486e392acf15505a73db2aef9be1647dc422d
Ali Mahdiyar <alimahdiyar77@gmail.com>
2018-07-29 13:24:15 +0430

    everything is messed up

---
+++ middlewares.js
@@ -0,0 +1,25 @@
+import { verifyJWTToken } from './libs/auth'
+
+export function verifyJWT_MW(req, res, next)
+{
+  let token = (req.method === 'POST') ? req.body.token : req.query.token
+
+  verifyJWTToken(token)
+    .then((decodedToken) =>
+    {
+      req.user = decodedToken.data
+      next()
+    })
+    .catch((err) =>
+    {
+      res.status(400)
+        .json({message: "Invalid auth token provided."})
+    })
+}
+
+
+//TODO: complete this function
+export function paramCheck(req, res, next)
+{
+    next();
+}
